36c6703ff1d320606ba87bb769df6aef21bac5bb
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro-2.local>
2016-11-05 15:33:40 -0400

    first block breaker commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextScene()
+	{
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex + 1);
+	}
+
+	public void PlayAgain(){
+		SceneManager.LoadScene (0);
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+}
